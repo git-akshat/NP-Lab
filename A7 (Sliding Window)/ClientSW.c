962f313f6368d5dc215f1ffdfaba152bab5969b3
Akshat Agarwal <mr.akshatagarwal26@gmail.com>
2019-12-21 07:21:49 +0530

    optimised/simplified

--- A7 (Sliding Window)/ClientSW.c
+++ A7 (Sliding Window)/ClientSW.c
@@ -18,19 +18,22 @@
 
 int main() 
 { 
-    int sock, ack; 
-    char msg[MAX], frame[MAX]; 
-
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr = { AF_INET, htons(1234), inet_addr("127.0.0.1") };
 
     /*  keep trying to establish connection with server */
     while(connect(sock, (struct sockaddr *) &addr, sizeof(addr))) ;
     printf("\nClient is connected to Server\n");
 
-    printf("\nEnter message to send : "); scanf("%s", msg);
+    char msg[MAX];
+    printf("\nEnter message to send : ");
+    fgets(msg, MAX, stdin);
+    msg[strlen(msg)-1] = '\0';
 
+    char frame[MAX];
     int i = 0;
+    int ack; 
+    
     while(i<strlen(msg)) 
     { 
         int sendsize = 5;
@@ -47,10 +50,8 @@ int main()
         printf("\n start seqno = %d", i);
         printf("\n end seqno   = %d", i+sendsize-1);
 
-        send(sock, frame, strlen(frame), 0); 
-        printf("\nFrame sent. Waiting for ack...");
-
-        recv(sock, &ack, sendsize, 0); 
+        send(sock, frame, sendsize, 0);
+        recv(sock, &ack, sizeof(ack), 0); 
         printf("\nreceived ack no = %d ",ack);  
 
         i = ack; // next data seq no = incoming ack no
@@ -58,3 +59,51 @@ int main()
     send(sock, "Exit", strlen("Exit"), 0); 
     close(sock); printf("\n\n");
 }
+
+/*
+Client is connected to Server
+
+Enter message to send : this is my house
+
+Sending packet = this 
+Sending window: 
+ start seqno = 0
+ end seqno   = 4
+received ack no = 3 
+
+Sending packet = s is 
+Sending window: 
+ start seqno = 3
+ end seqno   = 7
+received ack no = 6 
+
+Sending packet = s my 
+Sending window: 
+ start seqno = 6
+ end seqno   = 10
+received ack no = 7 
+
+Sending packet =  my h
+Sending window: 
+ start seqno = 7
+ end seqno   = 11
+received ack no = 10 
+
+Sending packet =  hous
+Sending window: 
+ start seqno = 10
+ end seqno   = 14
+received ack no = 12 
+
+Sending packet = ouse
+Sending window: 
+ start seqno = 12
+ end seqno   = 15
+received ack no = 14 
+
+Sending packet = se
+Sending window: 
+ start seqno = 14
+ end seqno   = 15
+received ack no = 16 
+*/
--- A7 (Sliding Window)/ServerSW.c
+++ A7 (Sliding Window)/ServerSW.c
@@ -19,25 +19,23 @@
 
 int main() 
 { 
-	int sersock, sock, reuse=1; 
-	char frame[MAX];
-	char res[MAX]; // to store all bytes that are recieved successfully
-	int ack;
-
-	sersock = socket(AF_INET, SOCK_STREAM, 0);
-	
+	int sersock = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in addr = { AF_INET, htons(1234), inet_addr("127.0.0.1") };
 
 	// Forcefully connecting to same port everytime
+	int reuse = 1;
 	setsockopt(sersock, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse));
 
 	bind(sersock, (struct sockaddr *) &addr, sizeof(addr));
-	printf("\nServer is Online");
+	printf("\nServer is Online\n");
 
 	listen(sersock, 5);
-	sock = accept(sersock, NULL, NULL);
+	int sock = accept(sersock, NULL, NULL);
 
-	int k=0; // iterator for res[MAX]
+	char frame[MAX];
+	char res[MAX]; // to store all bytes that are recieved successfully
+	int ack;
+	int k=0;	   // iterator for res
 	srand(time(NULL));
 	
 	while(1) 
@@ -46,46 +44,37 @@ int main()
 		memset(frame, 0, MAX); // re-initialise frame buffer with 0
 
 		recv(sock, frame, recvsize, 0);  // recv(socket, buffer, length, flag)
+		if(strcmp(frame, "Exit") == 0) break; // at end exit frame is recieved 
 
 		if(strlen(frame) < recvsize) 
 		{
 			recvsize = strlen(frame);
 		}
-		// at end exit frame is recieved from client
-		if(strcmp(frame, "Exit") == 0) break;
 		
 		int err_idx = rand()%8; // probability of byte to get corrupted = 50%
-		int j;
-
 		if(err_idx < recvsize)
 		{
-			for(j=0; j<err_idx ; j++)
-			{
-				res[k++] = frame[j];
-			}
+			recvsize = err_idx;
 			frame[err_idx]='x';
-			printf("\n\nPacket received = %s", frame);
-			printf("\nError at byte   = %d", err_idx+1);
-			printf("\nReceiving window: ");
-			printf("\n start seqno = %d", k-err_idx);
-			printf("\n end seqno   = %d", k-1);
+			printf("\nError occured at = %d", err_idx+1);
 		}
-		else
-		{ 
-			for(j=0; j<recvsize ; j++)
-			{
-				res[k++] = frame[j];
-			}
-			printf("\n\nPacket received = %s", frame);
-			printf("\nReceiving window: ");
-			printf("\n start seqno = %d", k-recvsize);
-			printf("\n end seqno   = %d", k-1);
+
+		int j;
+		for(j=0; j<recvsize ; j++)
+		{
+			res[k++] = frame[j];
 		}
+		printf("\nPacket received  = %s", frame);
+		printf("\nReceiving window: ");
+		printf("\n start seqno = %d", k-recvsize);
+		printf("\n end seqno   = %d", k-1);
+
 		ack = k ;
 		printf("\nSending ack = %d", ack);
 		send(sock, &ack, sizeof(ack), 0) ;  
+		printf("\n");
 	}
-	
+
 	res[k] = '\0';
 	printf("\n\nFinal string recieved at Destination = ");
 	fputs(res, stdout);
@@ -93,3 +82,58 @@ int main()
 	printf("\n\n");
 	close(sock); close(sersock);
 }
+
+/*
+Server is Online
+
+Error occured at = 4
+Packet received  = thix 
+Receiving window: 
+ start seqno = 0
+ end seqno   = 2
+Sending ack = 3
+
+Error occured at = 4
+Packet received  = s ix 
+Receiving window: 
+ start seqno = 3
+ end seqno   = 5
+Sending ack = 6
+
+Error occured at = 2
+Packet received  = sxmy 
+Receiving window: 
+ start seqno = 6
+ end seqno   = 6
+Sending ack = 7
+
+Error occured at = 4
+Packet received  =  myxh
+Receiving window: 
+ start seqno = 7
+ end seqno   = 9
+Sending ack = 10
+
+Error occured at = 3
+Packet received  =  hxus
+Receiving window: 
+ start seqno = 10
+ end seqno   = 11
+Sending ack = 12
+
+Error occured at = 3
+Packet received  = ouxe
+Receiving window: 
+ start seqno = 12
+ end seqno   = 13
+Sending ack = 14
+
+Packet received = se
+Receiving window: 
+ start seqno = 14
+ end seqno   = 15
+Sending ack = 16
+
+
+Final string recieved at Destination = this is my house
+*/
