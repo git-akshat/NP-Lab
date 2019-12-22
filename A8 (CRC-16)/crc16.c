2651123347914633b7e751898e37842e25173118
Akshat <mr.akshatagarwal26@gmail.com>
2019-12-22 22:37:32 +0530

    simplified crc

--- A8 (CRC-16)/crc16.c
+++ A8 (CRC-16)/crc16.c
@@ -11,23 +11,23 @@
 /* crc(dividend , divisor, remainder) */
 void crc(char *data, char *gen, char *rem)
 {
-    int i, j, lengthDiff = strlen(data)-strlen(gen) ; 
+    int i, j ;
+    int dwordSize = strlen(data)-(strlen(gen)-1) ; // dataword size
     char out[MAX]; // xored val after each step
 
     strcpy(out, data);
-
+    
     /* Perform XOR on the msg */
-    for(i=0; i<lengthDiff +1 ; i++)
+    for(i=0; i<dwordSize ; i++)
     {
-        if(out[i]=='0') continue ; 
-        out[i] = '0' ;
-        for(int j = 1 ; j<strlen(gen) ; j++){
-            out[i+j] = (gen[j]==out[i+j] ?'0':'1') ; 
+        if(out[i]=='0') continue ;
+        for(int j = 0 ; j<strlen(gen) ; j++){
+            out[i+j] = (gen[j] == out[i+j] ? '0' : '1') ; 
         }
     }
 
-    // size of output = strlen(gen)-1 = 16 bits 
-    strncpy(rem , out+lengthDiff+1 , strlen(gen)-1);
+    // strncpy(destination, start_idx, size) 
+    strncpy(rem , out+dwordSize , strlen(gen)-1); // rem = last strlen(gen)-1 bits of out
     rem[strlen(gen)-1] = '\0' ;
 }
 
@@ -39,7 +39,7 @@ int main()
     char cword[MAX]; // codeword
     char rem[MAX]; // remainder from crc
     char recv[MAX]; // received message 
-    char gen[MAX] = "10001000000100001\0";
+    char gen[MAX] = "10001000000100001"; // crc-16 (17 bits)
 
     printf("\nCRC-16 Generator : x^16 + x^12 + x^5 + 1 ");
     printf("\nBinary Form      : %s", gen);
@@ -71,11 +71,14 @@ int main()
     crc(recv, gen, rem);
 
     printf("\nSyndrome = %s ", rem);
-    
-    if(strstr( rem , "1")!=0)
-   {
-        printf("\nError occured !!! Corrupted data received. \n");
-        exit(0);
+
+    for(i=0; i<strlen(rem); i++)
+    {    
+        if( rem[i] == '1') 
+        {
+            printf("\nError occured !!! Corrupted data received.\n");
+            exit(0);
+        }
     }
     printf("\nNo Error. Data received successfully.\n");
 }
